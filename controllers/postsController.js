a129663a18d7258fb287be3ddb864b5f221ad55e
ruiming <ruiming.zhuang@gmail.com>
2016-10-10 16:27:26 +0800

    重构 postsController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -18,23 +18,22 @@ import _ from 'underscore';
  * @params: {string unread|mark} type
  */
 exports.list = async (ctx, next) => {
-    var user_id = ctx.state.user.id, type = ctx.request.query.type;
-    if(['mark', 'unread'].indexOf(type) === -1) {
-        ctx.throw(404, '不支持的查询类型');
-    } else {
-        var feeds = await UserFeedModel.find({user_id: user_id}, {feed_id: 1});
-        var posts = [];
+    let user_id = ctx.state.user.id, type = ctx.request.query.type;
+    if(['mark', 'unread'].includes(type)) {
+        let feeds = await UserFeedModel.find({user_id: user_id}, {feed_id: 1}), posts = [];
         await Promise.all(feeds.map(feed => new Promise(async (resolve, reject) => {
-            var query = {feed_id: feed.feed_id[0], user_id: user_id};
+            let query = {feed_id: feed.feed_id[0], user_id: user_id};
             type === 'mark' ? query['mark'] = true : query['read'] = true;
-            var result = await UserPostModel.find(query, {_id: 0, post_id: 1});
-            var data = _.invoke(_.flatten(_.pluck(result, 'post_id'), true), 'toString');
-            var items = await PostModel.find({feed_id: feed.feed_id}, {summary: 0, description: 0}).populate('feed_id', {_id: 1, title: 1, favicon: 1});
+            let result = await UserPostModel.find(query, {_id: 0, post_id: 1});
+            let data = _.invoke(_.flatten(_.pluck(result, 'post_id'), true), 'toString');
+            let items = await PostModel.find({feed_id: feed.feed_id}, {summary: 0, description: 0}).populate('feed_id', {_id: 1, title: 1, favicon: 1});
             if(type === 'mark') _.each(items, item => _.contains(data, item._id.toString()) ? posts.push(item) : _.noop());
             else _.each(items, item => !_.contains(data, item._id.toString()) ? posts.push(item) : _.noop());
             resolve();
         })));
         ctx.body = { success: true, data: posts };
+    } else {
+        ctx.throw(404, '不支持的类型查询');
     }
 
 }
@@ -45,27 +44,27 @@ exports.list = async (ctx, next) => {
  * @url:    /api/posts/recent
  */
 exports.main = async (ctx, next) => {
-    var user_id = ctx.state.user.id;
-    var items = await UserFeedModel.find({user_id: user_id}, {user_id: 0})
+    let user_id = ctx.state.user.id;
+    await UserFeedModel.find({user_id: user_id}, {user_id: 0})
         .populate('feed_id', {favicon: 1, title: 1}).lean().exec((err, items) => {
-            Promise.all(_.map(items, item => new Promise(async (resolve, reject) => {
-                var userposts = await UserPostModel.find({feed_id: item.feed_id, user_id: user_id, read: true});
+            return Promise.all(_.map(items, item => new Promise(async (resolve, reject) => {
+                let userposts = await UserPostModel.find({feed_id: item.feed_id, user_id: user_id, read: true});
                 await PostModel.find({feed_id: item.feed_id}).lean().exec((err, posts) => {
-                    var count = posts.length - userposts.length,
+                    let count = posts.length - userposts.length,
                     read_ids = _.invoke(_.pluck(userposts, 'post_id'), 'toString');
                     for(let post of posts) {
-                        if(read_ids.indexOf(post._id.toString()) === -1) {
+                        if(!read_ids.includes(post._id.toString())) {
                             post.summary = post.description.replace(/<[^>]+>/g,"").slice(0, 550);
                             post.description = post.description.match(/<img\s+src="(.*?)"/);
-                            // 图片处理
                             if(post.description) {
                                 if(post.description[1].slice(0, 2) !== '//' && post.description[1].slice(0, 2) !== 'ht') {
                                     post.description = post.website + post.description[1];
                                 } else {
                                     post.description = post.description[1];
                                 }
+                            } else {
+                                post.description = '/img/noimg.png';
                             }
-                            else   post.description = '/img/noimg.png';
                             resolve(Object.assign(post, item, {_id: post._id, unread: count}));
                             break;
                         }
@@ -84,14 +83,12 @@ exports.main = async (ctx, next) => {
  * @url:    /api/posts
  */
 exports.update = async (ctx, next) => {
-    var ids = ctx.request.body.id, user_id = ctx.state.user.id;
-    ids = ids.split(',');
-    console.log(user_id);
+    let ids = ctx.request.body.id.split(','), user_id = ctx.state.user.id, state, feed;
     UserFeedModel.update({user_id: user_id}, {$set: {unread: 0}}, {multi: true}).exec();
     _.each(ids, id => {
         setTimeout(async () => {
-            var state = await UserPostModel.findOne({user_id: user_id, post_id: id});
-            var feed = await PostModel.findOne({_id: id}, {_id: 1, feed_id: 1});
+            await Promise.all([Promise.resolve().then(async () => state = await UserPostModel.findOne({user_id: user_id, post_id: id})),
+                    Promise.resolve().then(async () => feed = await PostModel.findOne({_id: id}, {_id: 1, feed_id: 1}))]);
             if(state && state._id) {
                 state.read = true;
                 state.save();
