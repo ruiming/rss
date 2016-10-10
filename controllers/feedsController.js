fdd7db8fcbcd66582e5adc4883983fa74571af15
ruiming <ruiming.zhuang@gmail.com>
2016-10-10 19:01:01 +0800

    完善订阅源条件查询

--- controllers/feedsController.js
+++ controllers/feedsController.js
@@ -5,7 +5,8 @@ import _ from 'underscore';
  * 获取特定订阅源
  * @method: get
  * @url:    /api/feeds
- * @query:  {string} type
+ * @query:  {string} order
+ * @query:  {string} desc
  * @query:  {string} limit
  * @query:  {string} page
  * @query:  {string} per_page
@@ -13,10 +14,11 @@ import _ from 'underscore';
 
 exports.list = async (ctx, next) => {
     let user_id = ctx.state.user.id,
-        type = ctx.request.query.type,
+        order = ctx.request.query.order,
         limit = +ctx.request.query.limit,
         page = +ctx.request.query.page,
-        per_page = +ctx.request.per_page;
-    let result = await FeedModel.find().sort(type).skip(page*per_page).limit(per_page || limit);
+        per_page = +ctx.request.query.per_page,
+        desc = ctx.request.query.desc === 'true' ? '1' : '-1',
+        result = await FeedModel.find().sort({[order]: desc}).skip(page*per_page).limit(per_page || limit);
     ctx.body = result;
 }
