afa89a7a38fe759e89d76eedf9affb0586599344
ruiming <ruiming.zhuang@gmail.com>
2016-11-01 10:18:42 +0800

    fix index.js

--- index.js
+++ index.js
@@ -58,14 +58,8 @@ http.createServer(app.callback()).listen(config.PORT)
 if (config.ENV === 'production') {
     const options = {
         key: config.APP.SSL_KEY,
-<<<<<<< HEAD
         cert: config.APP.SSL_CERT,
         ca: config.APP.CA
-    };
-    http2.createServer(options, app.callback()).listen(443);
-=======
-        cert: config.APP.SSL_CERT
     }
-    http2.createServer(options, app.callback()).listen(443)
->>>>>>> vuex
+    http2.createServer(options, app.callback()).listen(443);
 }
