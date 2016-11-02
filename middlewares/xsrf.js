8989079421196bd783565ed783a7b727eac3965a
ruiming <ruiming.zhuang@gmail.com>
2016-11-02 20:58:00 +0800

    fix set of authorization header

--- middlewares/xsrf.js
+++ middlewares/xsrf.js
@@ -8,7 +8,7 @@ module.exports = function () {
     return async(ctx, next) => {
         let token = ctx.cookies.get('jwt'),
             xsrf = ctx.request.headers['x-xsrf-token']
-        if (ctx.request.header.authorization !== undefined) {
+        if (token !== undefined) {
             ctx.request.header.authorization = 'Bearer ' + token
         }
         if (undefined !== token && /^\/api\//.test(ctx.url)) {
