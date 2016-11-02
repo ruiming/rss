2a34cf35b4287fe7002a6857d9afba5dfde65dcb
ruiming <ruiming.zhuang@gmail.com>
2016-11-02 10:25:23 +0800

    [angular] remove angular response transformer

--- app/factory/tokenInjector.js
+++ app/factory/tokenInjector.js
@@ -9,44 +9,6 @@
 
         return {
 
-            response: function (config) {
-                let data = config.data.data
-                count[config.config.url] = undefined
-                if (Array.isArray(data)) {
-                    for (let i = 0, len = data.length; i < len; i++) {
-                        if (void 0 !== data[i].feed_id && Array.isArray(data[i].feed_id)) {
-                            // 提取 feed_id 
-                            if (typeof data[i].feed_id[0] === 'string') {
-                                config.data.data[i].feed_id = data[i].feed_id[0]
-                            } else {
-                                config.data.data[i] = Object.assign(data[i].feed_id[0], data[i], {
-                                    feed_id: data[i].feed_id[0]._id,
-                                    feed_title: data[i].feed_id[0].title
-                                })
-                            }
-                        }
-                        if (void 0 !== data[i].user_id && Array.isArray(data[i].user_id)) {
-                            config.data.data[i].user_id = data[i].user_id[0]
-                        }
-                    }
-                } else if (typeof data === 'object') {
-                    if (void 0 !== data.feed_id && Array.isArray(data.feed_id)) {
-                        if (typeof data.feed_id[0] === 'string') {
-                            config.data.data.feed_id = data.feed_id[0]
-                        } else {
-                            config.data.data = Object.assign(data.feed_id[0], data, {
-                                feed_id: data.feed_id[0]._id,
-                                feed_title: data.feed_id[0].title
-                            })
-                        }
-                    }
-                    if (void 0 !== data.user_id && Array.isArray(data.user_id)) {
-                        config.data.data.user_id = data.user_id[0]
-                    }
-                }
-                return $q.when(config)
-            },
-
             // 正常情况下，XSRF 不正确会触发该错误
             // 从而触发跳转到登录页面
             // TODO 全局提示弹框
