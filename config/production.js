b2e1a4d5475683c023e86abcf1c5b944c72d356d
ruiming <ruiming.zhuang@gmail.com>
2016-11-03 17:45:58 +0800

    test docker

--- config/production.js
+++ config/production.js
@@ -2,20 +2,21 @@ import fs from 'fs'
 import { SHA256 } from 'crypto-js'
 import _ from 'underscore'
 
+// TODO Docker Test
 module.exports = {
-    ENV:     'production',
+    ENV:     'development',
     PORT:    80,
     MONGODB: {
-        PORT:     27017,
-        HOST:     'localhost',
-        USER:     'root',
-        PASSWORD: 'root',
-        NAME:     'rss'
+        PORT:     process.env.MONGODB_PORT || 27017,
+        HOST:     process.env.MONGODB_HOST || 'localhost',
+        USER:     process.env.MONGODB_USERNAME || 'root',
+        PASSWORD: process.env.MONGODB_PASSWORD || 'root',
+        NAME:     process.env.MONGODB_DB || 'rss'
     },
     APP: {
         JWT_KEY:  SHA256(_.random(999999)).toString(),
-        SSL_KEY:  fs.readFileSync('/etc/letsencrypt/live/enjoyrss.com/privkey.pem'),
-        SSL_CERT: fs.readFileSync('/etc/letsencrypt/live/enjoyrss.com/cert.pem'),
-        CA:       fs.readFileSync('/etc/letsencrypt/live/enjoyrss.com/chain.pem')
+        SSL_KEY:  null,
+        SSL_CERT: null,
+        CA:       null
     }
 }
