247aa33a5315d24030509adba98cc55e796a2ba6
ruiming <ruiming.zhuang@gmail.com>
2016-11-14 11:23:05 +0800

    update cache-control

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     return async (ctx, next) => {
         await next()
-        if (/favicon/.test(ctx.request.path)) {
+        if (/favicon|img/.test(ctx.request.path)) {
             ctx.cacheControl = {
                 maxAge: 60 * 60 * 24 * 30
             }
