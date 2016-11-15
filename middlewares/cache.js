4cfac3c52b6a89e05bcfabaeb4ce6dcfc95a5947
ruiming <ruiming.zhuang@gmail.com>
2016-11-15 10:50:44 +0800

    update cache

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -5,14 +5,8 @@ module.exports = function () {
         // 只缓存 GET 请求
         if (ctx.request.method === 'GET') {
             if (/image/.test(ctx.type)) {
-                // 检查 if-none-match 头匹配 Etag
-                if (ctx.get('if-none-match') === SHA256(ctx.response.body).toString()) {
-                    ctx.status = 304
-                } else {
-                    // 控制客户端缓存时间 30 天
-                    ctx.cacheControl = {
-                        maxAge: 60 * 60 * 24 * 30
-                    }
+                ctx.cacheControl = {
+                    maxAge: 60 * 60 * 24 * 30
                 }
             }
         }
