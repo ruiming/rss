95a3debb623f0344dbbb3e941f91281126c4b9f1
ruiming <ruiming.zhuang@gmail.com>
2016-11-16 20:12:30 +0800

    update maxage to css or javascript file

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -3,12 +3,12 @@ import { SHA256 } from 'crypto-js'
 module.exports = function () {
     return async (ctx, next) => {
         if (ctx.request.method === 'GET') {
-            if (/favicon|image/.test(ctx.path)) {
+            if (/javascript|css|favicon|image/.test(ctx.path)) {
                 ctx.cacheControl = {
-                    maxAge: 60 * 60 * 24 * 30
+                    maxAge: 60 * 60 * 24 * 180
                 }
             }
-        } 
+        }
         await next()
     }
 }
