5aa4f3541e33625fac8901a59bbf7e0c835485b6
ruiming <ruiming.zhuang@gmail.com>
2016-11-17 23:36:19 +0800

    :lipstick: add style to router-link-active

--- vue/components/navbar.vue
+++ vue/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bottom" v-bind:class="{expand2: expand}">
     <ul class="list-group">
-        <router-link :to="{name: 'home'}"><span class="icon-book"><small>未读</small></span></router-link>
+        <router-link :to="{name: 'home'}" exact><span class="icon-book"><small>未读</small></span></router-link>
         <router-link :to="{name: 'feeds'}"><span class="icon-folder"><small>订阅源</small></span></router-link>
         <router-link :to="{name: 'mark'}"><span class="icon-star-full"><small>收藏</small></span></router-link>
         <router-link :to="{name: 'square'}"><span class="icon-pacman"><small>广场</small></span></router-link>
@@ -19,5 +19,7 @@ export default {
 </script>
 
 <style>
-
+.router-link-active span:before {
+    color: #353535;
+}
 </style>
