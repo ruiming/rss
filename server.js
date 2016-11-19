76420166d911c401611aa6fba513261c131d2b6a
ruiming <ruiming.zhuang@gmail.com>
2016-11-19 20:29:57 +0800

    :penguin: fix the warning of possible EventEmitter memory leak detected

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('babel-core/register')({
 })
 // stage-3 is not needed in node v7
 
+require('events').EventEmitter.prototype._maxListeners = 100
+
 require('babel-polyfill')
 
 require('./index.js')
