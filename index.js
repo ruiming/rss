d782f6baf8e712226153ec88943f2f5b6a59eddc
ruiming <ruiming.zhuang@gmail.com>
2017-04-03 14:39:57 +0800

    移除缓存,SSL等(由Nginx处理)

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 import Koa from 'koa'
-import http from 'http'
 import jwt from 'koa-jwt'
-import http2 from 'http2'
 import mongoose from 'mongoose'
-import enforceHttps from 'koa-sslify'
 import api from './routes/index'
 import handel from './routes/handel'
 import config from './config/config'
-import enforceWww from './middlewares/enforce-www'
 import onerror from './middlewares/onerror'
 import xsrf from './middlewares/xsrf'
 import cookies from './middlewares/cookies'
 import nghtml5 from './middlewares/nghtml5'
-import cache from './middlewares/cache'
 import ua from './middlewares/ua'
 import normal from './middlewares/normal'
 
@@ -24,8 +19,6 @@ app.use(onerror())
 
 if (config.ENV === 'production') {
   mongoose.connect(`mongodb://${config.MONGODB.USER}:${config.MONGODB.PASSWORD}@${config.MONGODB.HOST}:${config.MONGODB.PORT}/${config.MONGODB.NAME}`)
-  app.use(enforceHttps())
-  app.use(enforceWww())
 } else {
   mongoose.connect(`mongodb://${config.MONGODB.HOST}:${config.MONGODB.PORT}/${config.MONGODB.NAME}`)
 }
@@ -34,7 +27,6 @@ app.use(ua())
 app.use(cookies())
 app.use(normal())
 app.use(xsrf())
-app.use(cache())
 
 app.use(handel.routes())
     .use(handel.allowedMethods())
@@ -51,14 +43,4 @@ app.use(jwt({
 app.use(api.routes())
     .use(api.allowedMethods())
 app.use(nghtml5())
-http.createServer(app.callback()).listen(config.PORT)
-
-// Production Only
-if (config.ENV === 'production') {
-  const options = {
-    key: config.APP.SSL_KEY,
-    cert: config.APP.SSL_CERT,
-    ca: config.APP.CA
-  }
-  http2.createServer(options, app.callback()).listen(443)
-}
+app.listen(config.PORT)
--- middlewares/normal.js
+++ middlewares/normal.js
@@ -1,5 +1,4 @@
 import compose from 'koa-compose'
-import compress from 'koa-compress'
 import json from 'koa-json'
 import convert from 'koa-convert'
 import bodyparser from 'koa-bodyparser'
@@ -8,32 +7,20 @@ import serve from 'koa-static'
 import views from 'koa-views'
 import path from 'path'
 import favicon from 'koa-favicon'
-import cacheControl from 'koa-cache-control'
 import conditional from 'koa-conditional-get'
-import etag from 'koa-etag'
-import zlib from 'zlib'
 
 const normal = () => compose([
-  compress({
-    filter:    contentType => /text|application/i.test(contentType),
-    threshold: 2048,
-    flush:     zlib.Z_SYNC_FLUSH,
-  }),
   conditional(),
-  etag(),
   json(),
-  convert(cacheControl({
-    maxAge: 2,
-  })),
   convert(bodyparser()),
   convert(logger()),
   convert(serve(path.resolve(__dirname, '../public'), {
-    defer: true,
+    defer: true
   })),
   views(path.resolve(__dirname, '../views'), {
-    extension: 'ejs',
+    extension: 'ejs'
   }),
-  favicon(path.resolve(__dirname, '../public/img/rss.png')),
+  favicon(path.resolve(__dirname, '../public/img/rss.png'))
 ])
 
 export default normal
