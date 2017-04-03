b8b88dc12e73efa21d18d066829f2c0bf52b14ef
ruiming <ruiming.zhuang@gmail.com>
2017-04-03 15:13:11 +0800

    移除 cookie 的 secure 设置

--- middlewares/cookies.js
+++ middlewares/cookies.js
@@ -3,43 +3,39 @@ import { SHA256 } from 'crypto-js'
 import jwt from 'jsonwebtoken'
 import config from '../config/config'
 
-
 module.exports = () => async (ctx, next) => {
   ctx.clearcookies = () => {
     ctx.cookies.set('XSRF-TOKEN', null, {
       overwrite: true,
-      expires:   new Date(),
+      expires: new Date()
     })
     ctx.cookies.set('jwt', null, {
       overwrite: true,
-      expires:   new Date(),
+      expires: new Date()
     })
   }
   ctx.setAuthCookies = _id => {
     const xsrf = SHA256(_.random(999999999)).toString()
-    const secure = config.ENV === 'production'
     const date = new Date().getTime() + 5184000000
     const token = jwt.sign({
-      id:  _id,
+      id: _id,
       xsrf,
-      exp: date / 1000,
+      exp: date / 1000
     }, config.APP.JWT_KEY)
     ctx.cookies.set('XSRF-TOKEN', xsrf, {
-      httpOnly:  false,
+      httpOnly: false,
       overwrite: true,
-      expires:   new Date(date),
-      secure,
+      expires: new Date(date)
     })
     ctx.cookies.set('jwt', token, {
-      httpOnly:  true,
+      httpOnly: true,
       overwrite: true,
-      expires:   new Date(date),
-      secure,
+      expires: new Date(date)
     })
     return {
       jwt: token,
       xsrf,
-      exp: date / 1000,
+      exp: date / 1000
     }
   }
   await next()
